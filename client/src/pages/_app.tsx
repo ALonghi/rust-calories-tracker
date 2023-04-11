bdaa9f5420a53aa9275a03b2b15bfdee451bafcb
Alessio Longhi <alonghii@icloud.com>
2023-04-12 00:42:13 +0200

    Add icon

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,14 +5,12 @@ import React from "react";
 import Notifications from "@components/shared/Notifications/Notifications";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  console.log(`envs: ${JSON.stringify(process.env)}`);
-  console.log(`NEXT_PUBLIC_API_URL: ${process.env.NEXT_PUBLIC_API_URL}`);
   return (
     <>
       <Head>
-        <title>Kanban board</title>
+        <title>Calories tracker</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href="icons/favicon.svg" />
+        <link rel="shortcut icon" href="favicon.ico" />
       </Head>
       <div className="flex flex-row ">
         <Notifications />
